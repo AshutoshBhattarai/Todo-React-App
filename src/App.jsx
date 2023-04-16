1d0395921b274c81b668dc6cb837c859e1e54889
Ashutosh <ashutoshbhattarai90@gmail.com>
2023-04-16 10:02:25 +0545

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,28 @@ import Home from "./Routes/Home";
 import Page404 from "./Routes/Error/Page404";
 import CheckAuth from "./Components/CheckAuth";
 import Navbar from "./Components/NavBar";
+import { ErrorProvider } from "./Context/useError";
 function App() {
   return (
+    <ErrorProvider>
     <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/todo"
-            element={
-              <CheckAuth>
-                <Todo />
-              </CheckAuth>
-            }
-          ></Route>
-          <Route path="*" element={<Page404 />} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/todo"
+          element={
+            <CheckAuth>
+              <Todo />
+            </CheckAuth>
+          }
+        ></Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
     </>
+    </ErrorProvider>
   );
 }
 
---
+++ src/Context/useError.jsx
@@ -0,0 +1,18 @@
+import { React, createContext, useContext, useState } from "react";
+
+const ErrorContext = createContext(null);
+export const useErrorContext = () => {
+  return useContext(ErrorContext);
+};
+
+export const ErrorProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const errorDisplay = (err) => {
+    setError(err);
+  };
+  return (
+    <ErrorContext.Provider value={{ error, errorDisplay }}>
+      {children}
+    </ErrorContext.Provider>
+  );
+};
